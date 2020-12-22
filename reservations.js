396186a5c34f5d433da87ce74da87c43415f4887
Charli <charlispilecki@gmail.com>
2020-12-21 20:19:30 -0600

    added file for storing date and functions for storing and retrieving reservations

---
+++ reservations.js
@@ -0,0 +1,36 @@
+// Reservation Information (DATA)
+// =============================================================
+var reservations = [
+    {
+      name: "John Smith",
+      phone: "000-000-0000",
+      email: "john.s@gmail.com",
+      id: 1234
+    }
+  ];
+
+  var waitingList = [
+    {
+      name: "John Smith",
+      phone: "000-000-0000",
+      email: "john.s@gmail.com",
+      id: 1234
+    }
+  ];
+
+  
+  exports.addReservation = function(reservation){
+    if (reservations.length < 5){
+        reservations.push(reservation);  
+    }
+    else {
+        waitingList.push(reservation);
+    }
+  }
+
+exports.getReservations = function(){
+    return {
+        current: reservations,
+        waitingList: waitingList
+    }
+}
